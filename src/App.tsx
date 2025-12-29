360efd1fd033203f0feb69786a65d825087a3908
jst7501 <cfku0172@gmail.com>
2025-12-29 16:54:25 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,7 @@ function App() {
           )}
           {activeTab === "severance" && (
             <div className="w-full h-full overflow-y-auto">
-              <SeveranceCalculator lang={lang} />
+              <SeveranceCalculator />
             </div>
           )}
           {activeTab === "remit" && (
